dc90df31a2c28fd00cf02ad9c239926a6243e7ab
ZechB7 <117792233+ZechB7@users.noreply.github.com>
2023-03-20 13:52:13 -0500

    more setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const connection = require("./connection");
+
+const { prompt } = require("inquirer");
+const logo = require("asciiart-logo");
+const db = require("./db");
+require("console.table");
+
+init();
+
+function init() {
+  const logoText = logo({ name: "Employee Manager" }).render();
+
+  console.log(logoText);
+
+  prompts();
+}
+
+function prompts() {
+    
+}
