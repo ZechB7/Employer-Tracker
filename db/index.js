c40b71be0e847c6b381d196b554c6917ea463216
ZechB7 <117792233+ZechB7@users.noreply.github.com>
2023-03-20 14:59:39 -0500

    setup queries

--- db/index.js
+++ db/index.js
@@ -5,7 +5,90 @@ class DB {
   constructor(connection) {
     this.connection = connection;
   }
+//function(){
+//return this.connection.promise().query(
+    // query selector and params
+//);}
 
-//return this.connection.promise().query()
+//view all employees
+viewAllEmployees(){
+    return this.connection.promise().query(
+        " "
+    )
+}
+
+//view all employees by department
+viewAllEmployeesByDepartment(){
+    return this.connection.promise().query(
+        " "
+    )
+}
+//view all employees by manager
+viewAllEmployeesByManager(){
+    return this.connection.promise().query(
+        " "
+    )
+}
+//add employee
+viewAllEmployees(){
+    return this.connection.promise().query(
+        " "
+    )
+}
+//remove employee
+viewAllEmployees(){
+    return this.connection.promise().query(
+        " "
+    )
+}
+//update employee role
+UpdateEmployeeRole(){
+    return this.connection.promise().query(
+        " "
+    )
+}
+//view all roles
+viewAllRoles(){
+    return this.connection.promise().query(
+        " "
+    )
+}
+//add role
+addRole(){
+    return this.connection.promise().query(
+        " "
+    )
+}
+//remove role
+removeRole(){
+    return this.connection.promise().query(
+        " "
+    )
+}
+//view all departments
+viewAllDepartments(){
+    return this.connection.promise().query(
+        " "
+    )
+}
+//add department
+addDepartment(){
+    return this.connection.promise().query(
+        " "
+    )
+}
+//remove department
+removeDepartment(){
+    return this.connection.promise().query(
+        " "
+    )
+}
+// view total utilized budget by department
+viewDepartmentCost(){
+    return this.connection.promise().query(
+        " "
+    )
+}
 
 }
+module.exports = new DB(connection);
